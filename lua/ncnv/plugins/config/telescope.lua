ddfe600c0d1113b6ef04c0bfd4519b2db3af60b5
MorganKF <mfudge92@gmail.com>
2022-05-22 23:09:36 -0230

    Update mappings

--- lua/ncnv/plugins/config/telescope.lua
+++ lua/ncnv/plugins/config/telescope.lua
@@ -48,3 +48,8 @@ telescope.setup({
 })
 
 require('telescope').load_extension('fzf')
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, {})
