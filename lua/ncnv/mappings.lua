1237c7d5ac6c947de9c4faa4c9fd124fe62c8bbf
MorganKF <mfudge92@gmail.com>
2022-05-23 20:49:51 -0230

    Add buffer mappings
    Add hl disable mapping
    Add windows copy and cut mapping

--- lua/ncnv/mappings.lua
+++ lua/ncnv/mappings.lua
@@ -1,2 +1,14 @@
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
+
+-- Emulate windows copy and cut
+vim.keymap.set('v', '<C-c>', '"+y<CR>', { noremap = true })
+vim.keymap.set('v', '<C-x>', '"+d<CR>', { noremap = true })
+
+-- Disable hl with two esc
+vim.keymap.set('', '<esc><esc>', '<cmd>noh<CR><esc>', { noremap = true, silent = true })
+
+-- Buffer navigation
+vim.keymap.set('n', '<tab>', '<cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<S-tab>', '<cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'kb', '<cmd>pdelete<CR>', { noremap = true, silent = true })
