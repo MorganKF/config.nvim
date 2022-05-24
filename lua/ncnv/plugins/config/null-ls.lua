7d2d46e4e9e865786a28703d059d4846809d3ee9
MorganKF <mfudge92@gmail.com>
2022-05-24 14:54:46 -0230

    Add null-ls eslint sources

--- lua/ncnv/plugins/config/null-ls.lua
+++ lua/ncnv/plugins/config/null-ls.lua
@@ -10,6 +10,9 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.code_actions.gitsigns,
+        null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.formatting.eslint
     },
     on_attach = function(lsp_client, bufnr)
         if lsp_client.supports_method('textDocument/formatting') then
