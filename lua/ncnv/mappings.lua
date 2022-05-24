3093050551d9bd06d3e46899b8e664238ab6d7c7
MorganKF <mfudge92@gmail.com>
2022-05-24 14:36:36 -0230

    Update noh mapping

--- lua/ncnv/mappings.lua
+++ lua/ncnv/mappings.lua
@@ -6,7 +6,7 @@ vim.keymap.set('v', '<C-c>', '"+y<CR>', { noremap = true })
 vim.keymap.set('v', '<C-x>', '"+d<CR>', { noremap = true })
 
 -- Disable hl with two esc
-vim.keymap.set('', '<esc><esc>', '<cmd>noh<CR><esc>', { noremap = true, silent = true })
+vim.keymap.set('n', '<esc>', '<cmd>noh<CR><CR>', { noremap = true, silent = true })
 
 -- Buffer navigation
 vim.keymap.set('n', '<tab>', '<cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
