2f89e2e68387112ff28532fa1a94b00c456a640c
MorganKF <mfudge92@gmail.com>
2022-05-24 14:49:03 -0230

    Add split mappings

--- lua/ncnv/mappings.lua
+++ lua/ncnv/mappings.lua
@@ -1,14 +1,22 @@
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
+local opts = {noremap = true, silent = true}
+
 -- Emulate windows copy and cut
-vim.keymap.set('v', '<C-c>', '"+y<CR>', { noremap = true })
-vim.keymap.set('v', '<C-x>', '"+d<CR>', { noremap = true })
+vim.keymap.set('v', '<C-c>', '"+y<CR>', opts)
+vim.keymap.set('v', '<C-x>', '"+d<CR>', opts)
 
 -- Disable hl with two esc
-vim.keymap.set('n', '<esc>', '<cmd>noh<CR><CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<esc>', '<cmd>noh<CR><CR>', opts)
 
 -- Buffer navigation
-vim.keymap.set('n', '<tab>', '<cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<S-tab>', '<cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', 'kb', '<cmd>bdelete<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<tab>', '<cmd>BufferLineCycleNext<CR>', opts)
+vim.keymap.set('n', '<S-tab>', '<cmd>BufferLineCyclePrev<CR>', opts)
+vim.keymap.set('n', 'kb', '<cmd>bdelete<CR>', opts)
+
+-- Split navigation
+vim.keymap.set('n', '<C-up>', '<C-w><up>', opts)
+vim.keymap.set('n', '<C-down>', '<C-w><down>', opts)
+vim.keymap.set('n', '<C-right>', '<C-w><right>', opts)
+vim.keymap.set('n', '<C-left>', '<C-w><left>', opts)
