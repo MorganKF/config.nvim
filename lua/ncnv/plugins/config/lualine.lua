830c551a9f46467de4631a20e3dbb02a82815138
MorganKF <mfudge92@gmail.com>
2022-07-30 00:46:48 -0230

    Formatting

--- lua/ncnv/plugins/config/lualine.lua
+++ lua/ncnv/plugins/config/lualine.lua
@@ -4,24 +4,24 @@ if not present then
     return
 end
 
-lualine.setup {
-  options = {
-    icons_enabled = true,
-    theme = "github_dimmed",
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-    globalstatus = false,
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', {'diagnostics', sources = {'coc', 'nvim_diagnostic'}}},
-    lualine_c = {},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  tabline = {},
-  extensions = {}
-}
+lualine.setup({
+    options = {
+        icons_enabled = true,
+        theme = 'github_dimmed',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = false,
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', { 'diagnostics', sources = { 'coc', 'nvim_diagnostic' } } },
+        lualine_c = {},
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
+})
