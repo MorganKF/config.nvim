f004501b929400f26288e8b0386af2e93070542b
MorganKF <mfudge92@gmail.com>
2023-02-24 18:26:31 -0330

    Add deno fmt nullls config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,6 +21,11 @@ return {
                 sources = {
                     nls.builtins.formatting.prettierd,
                     nls.builtins.formatting.eslint_d.with(eslint_d_config),
+                    nls.builtins.formatting.deno_fmt.with({
+                        condition = function(utils)
+                            return utils.root_has_file({ 'deno.json', 'deno.jsonc' })
+                        end,
+                    }),
                     nls.builtins.formatting.stylua,
                     nls.builtins.diagnostics.flake8,
                     nls.builtins.diagnostics.eslint_d.with(eslint_d_config),
